ad6b0a2582059b6204b717ed4487678e10c40895
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-12-10 01:09:31 +0600

    first commit

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZgODiFGQHuBYdUjazoK_7BsJFxhJvKzk",
+  authDomain: "final-asgn-conceptual.firebaseapp.com",
+  projectId: "final-asgn-conceptual",
+  storageBucket: "final-asgn-conceptual.firebasestorage.app",
+  messagingSenderId: "20481196884",
+  appId: "1:20481196884:web:8a7a5dd0e2ce34ff283ec1",
+};
+
+export const app = initializeApp(firebaseConfig);
